a2bb8809510a16a5ac5149b44216386d8f982ed2
Viktor <novakovets@brights.com.ua>
2018-12-21 19:53:38 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+dfkjdfkjdfjg
+      </div>
+    );
+  }
+}
+
+export default App;
